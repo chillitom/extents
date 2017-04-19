8b5900d2bb1cef3ac721dc6d535c2b8b362a9ea9
Tom Rathbone <Tom Rathbone>
2017-04-19 22:35:30 +0100

    fix some corner cases

--- extents.cpp
+++ extents.cpp
@@ -9,37 +9,43 @@ using namespace std;
 map<int, int> points;
 
 void add_point(int point, int delta) {
-	auto it = points.lower_bound(point);
+    auto it = points.lower_bound(point);
+    if (it != points.end() && it->first == point) {
+        it->second += delta;
+    } else {
+        points.emplace_hint(it, point, delta);
+    }
+}
 
-	if (it != points.end() && it->first == point) {
-		it->second += delta;
-	} else {
-		points.emplace_hint(it, point, delta);
-	}
+int lookup(int p) {
+    auto it = points.lower_bound(p);
+    if(it != points.end() && it->first > p)
+        it--;
+    return it == points.end() ? 0 : it->second;
 }
 
 int main() {
-	ifstream in("extents.txt");
+    ifstream in("extents.txt");
+    add_point(-1, 0);
+    int a, b;
+    while(in >> a >> b) {
+        add_point(a, 1);
+        add_point(b + 1, -1);
+    }
 
-	int a, b;
-	while(in >> a >> b) {
-		add_point(a, 1);
-		add_point(b, -1);
-	}
+    int n = 0;
+    for(auto& i : points) {
+        n += i.second;
+        i.second = n;
+    }
 
-	int open = 0;
-	for(auto& i : points) {
-		open += i.second;
-		i.second = open;
-	}
+    ifstream numbers("numbers.txt");
 
-	ifstream numbers("numbers.txt");
+    int point;
+    while(numbers >> point) {
+        cout << lookup(point) << endl;
+    }
 
-	int point;
-	while(numbers >> point) {
-		cout << points.lower_bound(point)->second + 1 << endl;
-	}
-
-	return 0;
+    return 0;
 }
 
